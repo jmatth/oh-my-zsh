ea2a332bd9f6bb40c593c58dc16a3a41d444c03d
Josh Matthews <jmatth@nbcs.rutgers.edu>
2012-11-17 21:36:14 -0500

    adding gitroot function to the git plugin.

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -39,6 +39,9 @@ compdef _git gm=git-merge
 alias grh='git reset HEAD'
 alias grhh='git reset HEAD --hard'
 
+# cd to the root of the repo
+alias groot='cd $(git rev-parse --show-toplevel)'
+
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
 compdef git-svn-dcommit-push=git
