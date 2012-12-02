5dc4b38ec63017e15ee5547f18ed39ca17ed3eb7
Josh Matthews <jmatth@nbcs.rutgers.edu>
2012-12-02 00:19:44 -0500

    Updating keyswap plugin for new layout.

--- custom/plugins/keyswap/keyswap.plugin.zsh
+++ custom/plugins/keyswap/keyswap.plugin.zsh
@@ -4,24 +4,36 @@ function keyswap () {
 	if ! (($+1))
 	then
 		xmodmap -e 'remove Lock = Caps_Lock' \
-		-e 'keycode 66 = Escape' \
-		-e 'keycode 9 = Caps_Lock' \
+		-e 'remove Control = Control_L' \
+		-e 'keycode 66 = Control_L' \
+		-e 'keycode 37 = Caps_Lock' \
+		-e 'add Control = Control_L' \
+		-e 'add Lock = Caps_Lock' \
 		-e 'keycode 49 = grave asciitilde grave asciitilde'
 
 	# Swapping for the kbt pure
 	elif [[ $1 == "-p" ]]
 	then
 		xmodmap -e 'remove Lock = Caps_Lock' \
+		-e 'remove Control = Control_L' \
+		-e 'keycode 66 = Caps_Lock' \
+		-e 'keycode 37 = Control_L' \
+		-e 'add Control = Control_L' \
+		-e 'add Lock = Caps_Lock' \
 		-e 'keycode 9 = grave asciitilde grave asciitilde' \
-		-e 'keycode 49 = Caps_Lock' \
-		-e 'keycode 66 = Escape'
+		-e 'keycode 49 = Escape' \
 
 	# Reset everything
 	elif [[ $1 == "-r" ]]
 	then
-		xmodmap -e 'keycode 66 = Caps_Lock' \
-		-e 'keycode 9 = Escape' \
+		xmodmap -e 'remove Lock = Caps_Lock' \
+		-e 'remove Control = Control_L' \
+		-e 'keycode 66 = Caps_Lock' \
+		-e 'keycode 37 = Control_L' \
+		-e 'add Control = Control_L' \
+		-e 'add Lock = Caps_Lock' \
 		-e 'keycode 49 = grave asciitilde grave asciitilde'
+		-e 'keycode 9 = Escape'
 
 	# Weird flag, print help
 	else
